088c4f1a2b7f355b5204309bb6436cac30c81c20
AlondraMin <31377903+AlondraMin@users.noreply.github.com>
2017-10-18 13:30:23 -0500

    Add files via upload
    Trabajo 1

---
+++ SumaPares.cc
@@ -0,0 +1,19 @@
+//Programa que suma los numeros pares entre el 1 y 20
+//Autor: Alondra Dominguez Gonzalez
+//Fecha: Octubre 16, 2017
+#include<iostream>
+using namespace std; 
+
+int main ( )
+{
+ int sum=0;
+ int count=1;
+ while(count<=20 ){
+      if ((count%2)==0){
+         sum=sum+count;
+      }
+         count++;
+}
+cout<<sum<<endl;
+ return 0;
+}
